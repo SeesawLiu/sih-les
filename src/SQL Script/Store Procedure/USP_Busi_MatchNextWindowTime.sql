6db66f747e3613f686bf25e2d84d2bdd5d839895
dingxin <novthirteen@gmail.com>
2013-11-15 09:41:42 +0800

    修改工作日历跨天取窗口时间出错的BUG

--- src/SQL Script/Store Procedure/USP_Busi_MatchNextWindowTime.sql
+++ src/SQL Script/Store Procedure/USP_Busi_MatchNextWindowTime.sql
@@ -23,13 +23,17 @@ BEGIN
 	set nocount on
 	if ISNULL(@WinTime, '') <> ''
 	begin
+		declare @WorkingDateStartTimeMi int
+		exec USP_Busi_GetStartTimeAsMinute @WorkingDateStartTimeMi output
+	
 		declare @SplitSymbol1 char(1) = ','
 		declare @SplitSymbol2 char(1) = '|'
 		
 		Create table #tempWinTime
 		(
 			WinTime varchar(5),
-			WinDateTime datetime
+			WinDateTime datetime,
+			WinMi int    --记录当天的第几分钟
 		)
 	
 		if (charindex(@SplitSymbol1, @WinTime) <> 0)
@@ -52,7 +56,14 @@ BEGIN
 		end
 
 		insert #tempWinTime(WinTime) values (Rtrim(Ltrim(@WinTime)))
-		update #tempWinTime set WinDateTime = CONVERT(DATETIME, CONVERT(varchar(10), @WinDate, 120) + ' ' + substring(WinTime, 1, charindex(':', WinTime) - 1) + ':' + substring(WinTime, charindex(':', WinTime) + 1, 2))
+		delete from #tempWinTime where WinTime = ''
+		
+		update #tempWinTime set WinDateTime = CONVERT(DATETIME, CONVERT(varchar(10), @WinDate, 120) + ' ' + substring(WinTime, 1, charindex(':', WinTime) - 1) + ':' + substring(WinTime, charindex(':', WinTime) + 1, 2)),
+		WinMi = convert(int, substring(WinTime, 1, charindex(':', WinTime) - 1)) * 60 + convert(int, substring(WinTime, charindex(':', WinTime) + 1, 2))
+		
+		--如果当天的分钟数小于每天的开始时间的分钟数，窗口时间+1天
+		update #tempWinTime set WinDateTime = DATEADD(DAY, 1, WinDateTime) where WinMi < @WorkingDateStartTimeMi
+		
 		select top 1 @NextTime = WinDateTime from #tempWinTime where WinDateTime > @BaseTime order by WinDateTime asc
 		
 		drop table #tempWinTime

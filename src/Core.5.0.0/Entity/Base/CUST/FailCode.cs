af07dd0d567dab656c9590aed3c4a1c110d4f82b
tanyongji <tanyongji@sconit.com>
2013-11-26 11:27:15 +0800

    新增工位映射

--- src/Core.5.0.0/Entity/Base/CUST/FailCode.cs
+++ src/Core.5.0.0/Entity/Base/CUST/FailCode.cs
@@ -7,31 +7,31 @@ namespace com.Sconit.Entity.CUST
     public partial class FailCode : EntityBase, IAuditable
     {
         #region O/R Mapping Properties
-         [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
-      [Display(Name = "FailCode_Code", ResourceType = typeof(Resources.CUST.FailCode))]
-		public string Code { get; set; }
-     [Display(Name = "FailCode_CHNDescription", ResourceType = typeof(Resources.CUST.FailCode))]
-		public string CHNDescription { get; set; }
-      [Display(Name = "FailCode_ENGDescription", ResourceType = typeof(Resources.CUST.FailCode))]
-		public string ENGDescription { get; set; }
-		//[Display(Name = "CreateUserId", ResourceType = typeof(Resources.CUST.FailCode))]
-		public Int32 CreateUserId { get; set; }
-		//[Display(Name = "CreateUserName", ResourceType = typeof(Resources.CUST.FailCode))]
-		public string CreateUserName { get; set; }
-		//[Display(Name = "CreateDate", ResourceType = typeof(Resources.CUST.FailCode))]
-		public DateTime CreateDate { get; set; }
-		//[Display(Name = "LastModifyUserId", ResourceType = typeof(Resources.CUST.FailCode))]
-		public Int32 LastModifyUserId { get; set; }
-		//[Display(Name = "LastModifyUserName", ResourceType = typeof(Resources.CUST.FailCode))]
-		public string LastModifyUserName { get; set; }
-		//[Display(Name = "LastModifyDate", ResourceType = typeof(Resources.CUST.FailCode))]
-		public DateTime LastModifyDate { get; set; }
-        
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        [Display(Name = "FailCode_Code", ResourceType = typeof(Resources.CUST.FailCode))]
+        public string Code { get; set; }
+        [Display(Name = "FailCode_CHNDescription", ResourceType = typeof(Resources.CUST.FailCode))]
+        public string CHNDescription { get; set; }
+        [Display(Name = "FailCode_ENGDescription", ResourceType = typeof(Resources.CUST.FailCode))]
+        public string ENGDescription { get; set; }
+        //[Display(Name = "CreateUserId", ResourceType = typeof(Resources.CUST.FailCode))]
+        public Int32 CreateUserId { get; set; }
+        //[Display(Name = "CreateUserName", ResourceType = typeof(Resources.CUST.FailCode))]
+        public string CreateUserName { get; set; }
+        //[Display(Name = "CreateDate", ResourceType = typeof(Resources.CUST.FailCode))]
+        public DateTime CreateDate { get; set; }
+        //[Display(Name = "LastModifyUserId", ResourceType = typeof(Resources.CUST.FailCode))]
+        public Int32 LastModifyUserId { get; set; }
+        //[Display(Name = "LastModifyUserName", ResourceType = typeof(Resources.CUST.FailCode))]
+        public string LastModifyUserName { get; set; }
+        //[Display(Name = "LastModifyDate", ResourceType = typeof(Resources.CUST.FailCode))]
+        public DateTime LastModifyDate { get; set; }
+
         #endregion
 
-		public override int GetHashCode()
+        public override int GetHashCode()
         {
-			if (Code != null)
+            if (Code != null)
             {
                 return Code.GetHashCode();
             }
@@ -53,7 +53,7 @@ namespace com.Sconit.Entity.CUST
             {
                 return (this.Code == another.Code);
             }
-        } 
+        }
     }
-	
+
 }
--- src/Core.5.0.0/Entity/Base/CUST/OpRefMap.cs
+++ src/Core.5.0.0/Entity/Base/CUST/OpRefMap.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using com.Sconit.Entity.SYS;
 
 namespace com.Sconit.Entity.CUST
 {
@@ -9,20 +10,46 @@ namespace com.Sconit.Entity.CUST
         #region O/R Mapping Properties
 		
 		public Int32 Id { get; set; }
-		public string SAPProdLine { get; set; }
-		public string ProdLine { get; set; }
-		public string Item { get; set; }
-		public string ItemDesc { get; set; }
-		public string ItemRefCode { get; set; }
-		public string OpReference { get; set; }
-		public string RefOpReference { get; set; }
-		public Boolean? IsPrimary { get; set; }
-		public Int32? CreateUserId { get; set; }
-		public string CreateUserName { get; set; }
-		public DateTime? CreateDate { get; set; }
-		public Int32? LastModifyUserId { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 10)]
+        [Display(Name = "OpRefMap_SAPProdLine", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        public string SAPProdLine { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 20)]
+        [Display(Name = "OpRefMap_ProdLine", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        public string ProdLine { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 30)]
+        [Display(Name = "OpRefMap_Item", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        public string Item { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 50)]
+        [Display(Name = "OpRefMap_ItemDesc", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        public string ItemDesc { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 40)]
+        [Display(Name = "OpRefMap_ItemRefCode", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        public string ItemRefCode { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 60)]
+        [Display(Name = "OpRefMap_OpReference", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        public string OpReference { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 70)]
+        [Display(Name = "OpRefMap_RefOpReference", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        public string RefOpReference { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 80)]
+        [Display(Name = "OpRefMap_IsPrimary", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Errors_Common_FieldRequired", ErrorMessageResourceType = typeof(Resources.ErrorMessage))]
+        public Boolean? IsPrimary { get; set; }
+		public Int32 CreateUserId { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 90)]
+        [Display(Name = "OpRefMap_CreateUserName", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        public string CreateUserName { get; set; }
+        [Export(ExportName = "OpRefMapXls", ExportSeq = 100)]
+        [Display(Name = "OpRefMap_CreateDate", ResourceType = typeof(Resources.CUST.OpRefMap))]
+        public DateTime CreateDate { get; set; }
+		public Int32 LastModifyUserId { get; set; }
 		public string LastModifyUserName { get; set; }
-		public DateTime? LastModifyDate { get; set; }
+		public DateTime LastModifyDate { get; set; }
         
         #endregion
 

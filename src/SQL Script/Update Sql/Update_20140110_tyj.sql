d5a9ddd1e0fc55dfe4311d44b304bb349cbe6744
谭永吉 <tanyongji@sconit.com>
2014-01-10 08:50:42 +0800

    脚本

---
+++ src/SQL Script/Update Sql/Update_20140110_tyj.sql
@@ -0,0 +1,3 @@
+insert into SYS_Menu values('Url_OrderMstr_Production_CKDProductOrder','导入CKD生产单','Url_OrderMstr_Production',109,'导入CKD生产单','~/ProductionOrder/CKDProductOrder','~/Content/Images/Nav/Default.png',1)
+go
+insert into ACC_Permission values('Url_OrderMstr_Production_CKDProductOrder','导入CKD生产单','Production')
